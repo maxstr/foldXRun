ae42fcdd6173a430f27cc4ac141d580f82916fa0
Max Stritzinger <max@lambda.mu>
2016-08-03 00:47:57 -0600

    FoldXRun v2.0

--- foldXRun.py
+++ foldXRun.py
@@ -3,37 +3,20 @@ from fabric.api import local, env, settings, lcd, hide
 import re
 from tempfile import mkdtemp
 from shutil import rmtree, copy
-from os.path import isfile, join, normpath, exists, abspath, dirname, basename
+from os.path import isfile, join, normpath, exists, abspath, dirname, basename, splitext
 import sys
 from os import listdir, makedirs
 import numpy
 import csv
 
-defaultOptions = """\
-<OPTIONS>FOLDX_optionfile;
-<Temperature>298;
-<R>#;
-<pH>7;
-<IonStrength>0.050;
-<water>-CRYSTAL;
-<metal>-CRYSTAL;
-<VdWDesign>2;
-<OutPDB>true;
-<pdb_hydrogens>false;
-<END>#;"""
-defaultJob = """\
-<JOBSTART>#;
-<PDBS>%(pdbsString)s;
-<BATCH>#;
-<COMMANDS>FOLDX_commandfile;
-%(commandsString)s
-<END>#;
-%(optionsString)s
-<JOBEND>#;"""
-defaultRunFile = """\
-<TITLE>FOLDX_runscript;
-%s
-<ENDFILE>#;"""
+repairFile = """\
+command=RepairPDB
+pdb=%s
+"""
+stabilizeFile = """\
+command=Stability
+pdb=%s
+"""
 
 def foldXRun(seqDirectory, mutDirectory, nativePDB, foldXPath = '.', outputDir = '.'):
 # First make sure data directory exists and is writeable
@@ -71,66 +54,65 @@ def foldXRun(seqDirectory, mutDirectory, nativePDB, foldXPath = '.', outputDir =
     copy(nativePDB, join(scratchDir, "native.pdb"))
     copy(join(dirname(foldXPath), 'rotabase.txt'), scratchDir)
 
-    # Now we setup the repair job
-    repairDict = {'optionsString':defaultOptions}
 
     # Setup repair PDBs
-    repairJobPDBs = ""
+    repairJobPDBs = []
     for modelFile in seqModels:
-        repairJobPDBs += "seq_%s," % modelFile
+        repairJobPDBs.append("seq_%s" % modelFile)
     for modelFile in mutModels:
-        repairJobPDBs += "mut_%s," % modelFile
-    repairJobPDBs += "native.pdb"
-    repairDict['pdbsString'] = repairJobPDBs
+        repairJobPDBs.append("mut_%s" % modelFile)
+    repairJobPDBs.append("native.pdb")
+    with open(join(scratchDir, "repairPDBs"), 'w') as f:
+        f.write("\n".join(repairJobPDBs))
 
-    # Setup repair commands
-
-    repairCommand = "<RepairPDB>#;"
-    repairDict['commandsString'] = repairCommand
-
-    # Finalize repair job.
-    repairJob = defaultJob % repairDict
 
     # Now we handle the stability job in the same way.
 
-    stabilizeDict = { 'optionsString':defaultOptions }
 
     # Setup stabilize PDBs
-    stabilizeJobPDBs = ""
+    stabilizeJobPDBs = []
     for modelFile in seqModels:
-        stabilizeJobPDBs += "RepairPDB_seq_%s," % modelFile
+        stabilizeJobPDBs.append("seq_%s_Repair.pdb" % splitext(modelFile)[0])
     for modelFile in mutModels:
-        stabilizeJobPDBs += "RepairPDB_mut_%s," % modelFile
-    stabilizeJobPDBs += "native.pdb"
-    stabilizeDict['pdbsString'] = stabilizeJobPDBs
+        stabilizeJobPDBs.append("mut_%s_Repair.pdb" % splitext(modelFile)[0])
+    stabilizeJobPDBs.append("native_Repair.pdb")
+    with open(join(scratchDir, "stabilizePDBs"), 'w') as f:
+        f.write("\n".join(stabilizeJobPDBs))
 
-    # Setup stabilize commands
+    # Setup report file list
+    reportSuffix = "_0_ST.fxout"
+    reportFiles = map(lambda pdb: splitext(pdb)[0] + reportSuffix,  stabilizeJobPDBs)
+    
 
-    stabilizeCommand = "<Stability>%s;" % join(dataDir, 'energyOutput.txt')
-    stabilizeDict['commandsString'] = stabilizeCommand
 
-    # Finalize stabilize job.
-    stabilizeJob = defaultJob % stabilizeDict
-    allJobs = repairJob + "\n" + stabilizeJob
-    repairRunFileText = defaultRunFile % repairJob
-    stabilizeRunFileText = defaultRunFile % stabilizeJob
-    with open(join(scratchDir, 'repairRunFile.txt'), 'w') as repair:
-        with open(join(scratchDir, 'stabilizeRunFile.txt'), 'w') as stabilize:
+    repairRunFileText = repairFile
+    stabilizeRunFileText = stabilizeFile
+    with open(join(scratchDir, 'repairRunFile.cfg'), 'w') as repair:
+        with open(join(scratchDir, 'stabilizeRunFile.cfg'), 'w') as stabilize:
             repair.write(repairRunFileText)
             stabilize.write(stabilizeRunFileText)
     with lcd(scratchDir):
-        with hide('everything'):
+#        with hide('everything'):
             print "Running repair job."
-            local("%s -runfile repairRunFile.txt" % foldXPath)
+            print repairJobPDBs
+            for repairJobPDB in repairJobPDBs:
+                local("%s --command=RepairPDB --pdb=%s" % (foldXPath, repairJobPDB))
             print "Running stabilize job."
-            local("%s -runfile stabilizeRunFile.txt" % foldXPath)
+            for stabilizeJobPDB in stabilizeJobPDBs:
+                local("%s --command=Stability --pdb=%s" % (foldXPath, stabilizeJobPDB))
             print "Done~"
 
     print results
     print "FoldX Complete - Now running analysis on output"
-    reportAnalyze(join(dataDir, 'energyOutput.txt'), join(dataDir, 'report.txt'), basename(normpath(seqDirectory)))
+    reportPaths = map(lambda reportFile: join(dataDir, reportFile), reportFiles)
+    reportAnalyze(scratchDir, join(dataDir, 'report.txt'), basename(normpath(seqDirectory)))
 
 def reportAnalyze(reportPath, outputPath, reportName):
+    # From  http://foldxsuite.crg.eu/command/Stability#inout-parameters
+    fieldHeaders = ["name", "total energy", "Backbone Hbond", "Sidechain Hbond", "Van der Waals", "Electrostatics", "Solvation Polar", "Solvation Hydrophobic", "Van der Waals clashes", \
+               "Entropy Side Chain", "Entropy Main Chain", "Sloop Entropy", "Mloop Entropy", "Cis Bond", "Torsional Clash", "Backbone Clash", "Helix Dipole", "Water Bridge", \
+               "Disulfide", "Electrostatic Kon", "Partial Covalent Bonds", "Energy Ionisation", "Entropy Complex", "Residue Number"]
+
     native = None
     seqs = []
     muts = []
@@ -138,33 +120,42 @@ def reportAnalyze(reportPath, outputPath, reportName):
     mutsSTDs = {}
     seqsMeans = {}
     mutsMeans = {}
+
+    
+    reportFiles = [f for f in listdir(reportPath) if re.search(".*_ST.fxout", f) and isfile(join(reportPath, f))]
+    print reportPath
+    print reportFiles
+
     # Reading in values from the Stabilized report
-    with open(reportPath, 'r') as f:
-        for i in range(8):
-            f.readline()
-        reader = csv.DictReader(f, delimiter='\t')
-        for row in reader:
-            if re.match(".*mut", row['']):
+    for reportFile in reportFiles:
+        with open(join(reportPath, reportFile), 'r') as f:
+            reader = csv.DictReader(f, delimiter='\t', fieldnames=fieldHeaders)
+            row = reader.next()
+            if re.match(".*mut", row["name"]):
                muts.append(row)
-            elif re.match(".*seq", row['']):
+            elif re.match(".*seq", row["name"]):
                 seqs.append(row)
             else:
                 native = row
 
     # Python is really bad!!
-    for i in seqs:
-        i['name'] = i['']
-        del i['']
-    for i in muts:
-        i['name'] = i['']
-        del i['']
-    native['name'] = native['']
-    del native['']
+#    for i in seqs:
+#        i['name'] = i['']
+#        del i['']
+#    for i in muts:
+#        i['name'] = i['']
+#        del i['']
+#    native['name'] = native['']
+#    del native['']
+
+    print seqs
+    print muts
+    print native
 
     # Calculate means, stds for each value
     for key in seqs[0]:
         # We don't want stats on these!
-        if key in ('name', 'Number of Residues'):
+        if key in ('name', 'Residue Number'):
             continue
         seqsList = [float(d[key]) for d in seqs]
         mutsList = [float(d[key]) for d in muts]
@@ -203,13 +194,12 @@ Analysis Output for %s\n\n""" % reportName)
 
 
 if __name__ == '__main__':
-    try:
-        foldXRun(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
-    except:
-        print """\
-Incorrect number of arguments entered!
-Usage: ./foldXRun.py seqsDir mutsDir nativePDB pathToFoldX outputDir\n"""
-        sys.exit(0)
+    foldXRun(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+#    except:
+#        print """\
+#Incorrect number of arguments entered!
+#Usage: ./foldXRun.py seqsDir mutsDir nativePDB pathToFoldX outputDir\n"""
+#        sys.exit(0)
 
 
 
